4755f44ac23c9cb7b7eb25e434197e8bff526980
Simon Li <orpheus+devel@gmail.com>
2016-10-12 23:54:43 +0100

    Add DockerServiceSpawner to __init__

--- dockerspawner/__init__.py
+++ dockerspawner/__init__.py
@@ -1,5 +1,11 @@
 from ._version import __version__
 from .dockerspawner import DockerSpawner
+from .dockerservicespawner import DockerServiceSpawner
 from .systemuserspawner import SystemUserSpawner
 
-__all__ = ['__version__', 'DockerSpawner', 'SystemUserSpawner']
+__all__ = [
+    '__version__',
+    'DockerSpawner',
+    'DockerServiceSpawner',
+    'SystemUserSpawner'
+]
